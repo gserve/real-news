210b95b3377789640861f700da4785ff47c58c01
gserve <98937512+gserve@users.noreply.github.com>
2022-04-21 06:25:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ const Home = () =>{
   const [blogs, setblogs] = useState ([
 
 
-    {title:'Suspect in Brooklyn subway train shooting called in the tip that led to his arrest, sources say - CNN',author:' Brynn Gingras',img:'https://raw.githubusercontent.com/gserve/real-news/main/public/images/breakingNews2.gif',link:'https://www.cnn.com/2022/04/13/us/brooklyn-subway-shooting-wednesday/index.html'},
+    {title:'Suspect in Brooklyn subway train shooting called in the tip that led to his arrest, sources say - CNN',author:' Brynn Gingras',img:'breakingNews2.gif',link:'https://www.cnn.com/2022/04/13/us/brooklyn-subway-shooting-wednesday/index.html'},
 
     {title:'Russian sailors evacuate warship in the Black Sea after Ukraine attack; U.S. will send another $800 million in weapons to Ukraine - CNBC',author:'Ted Kemp',img:"https://raw.githubusercontent.com/gserve/real-news/main/public/images/breakingNews2.gif",link:'https://www.cnbc.com/2022/04/13/russia-ukraine-live-updates.html'},
-    {title:'Effort to Remove Marjorie Taylor Greene From Ballot Can Proceed, Judge Says',author:'Thomas L. Friedman',img:'https://raw.githubusercontent.com/gserve/real-news/main/public/images/breakingNews2.gif',link:'https://www.nytimes.com/2022/04/18/opinion/china-russia-putin.html'},
+    {title:'Effort to Remove Marjorie Taylor Greene From Ballot Can Proceed, Judge Says',author:'Thomas L. Friedman',img:'breakingNews2',link:'https://www.nytimes.com/2022/04/18/opinion/china-russia-putin.html'},
 
   ]);
 
