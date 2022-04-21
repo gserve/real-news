742deaddf8f46cc51440406e0586bd494e874ef0
gserve <98937512+gserve@users.noreply.github.com>
2022-04-21 05:17:05 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ const Home = () =>{
   const [blogs, setblogs] = useState ([
 
 <<<<<<< HEAD
-    {title:'Suspect in Brooklyn subway train shooting called in the tip that led to his arrest, sources say - CNN',author:' Brynn Gingras',img:"https://media.giphy.com/media/3o6ZthWzyoIzSknltu/giphy.gif",link:'https://www.cnn.com/2022/04/13/us/brooklyn-subway-shooting-wednesday/index.html'},
+    {title:'Suspect in Brooklyn subway train shooting called in the tip that led to his arrest, sources say - CNN',author:' Brynn Gingras',img:'https://i.postimg.cc/TYvNnHfN/breaking-News2.gif',link:'https://www.cnn.com/2022/04/13/us/brooklyn-subway-shooting-wednesday/index.html'},
 =======
-    {title:'Suspect in Brooklyn subway train shooting called in the tip that led to his arrest, sources say - CNN',author:' Brynn Gingras',img:'https://media.giphy.com/media/3o6ZthWzyoIzSknltu/giphy.gif',link:'https://www.cnn.com/2022/04/13/us/brooklyn-subway-shooting-wednesday/index.html'},
+    {title:'Suspect in Brooklyn subway train shooting called in the tip that led to his arrest, sources say - CNN',author:' Brynn Gingras',img:'https://i.postimg.cc/TYvNnHfN/breaking-News2.gif',link:'https://www.cnn.com/2022/04/13/us/brooklyn-subway-shooting-wednesday/index.html'},
 >>>>>>> c5a59eba0ff27f0dc948020ec3e44b8c0ec0b494
     {title:'Russian sailors evacuate warship in the Black Sea after Ukraine attack; U.S. will send another $800 million in weapons to Ukraine - CNBC',author:'Ted Kemp',img:"https://media.giphy.com/media/3o6ZthWzyoIzSknltu/giphy.gif"
       ,link:'https://www.cnbc.com/2022/04/13/russia-ukraine-live-updates.html'},
-    {title:'Effort to Remove Marjorie Taylor Greene From Ballot Can Proceed, Judge Says',author:'Thomas L. Friedman',img:'https://media.giphy.com/media/3o6ZthWzyoIzSknltu/giphy.gif',link:'https://www.nytimes.com/2022/04/18/opinion/china-russia-putin.html'},
+    {title:'Effort to Remove Marjorie Taylor Greene From Ballot Can Proceed, Judge Says',author:'Thomas L. Friedman',img:'https://i.postimg.cc/TYvNnHfN/breaking-News2.gif',link:'https://www.nytimes.com/2022/04/18/opinion/china-russia-putin.html'},
 
   ]);
 
@@ -29,7 +29,7 @@ const Home = () =>{
           <p>{<img src= {blog.img} alt=' '/>}</p>
 =======
                       
-          <p{<img src= {blog.img} alt='https://postimg.cc/Zv89wz3H '/></p>
+          <p{<img src= {blog.img} alt='BreakingNews2'/></p>
 
 >>>>>>> c5a59eba0ff27f0dc948020ec3e44b8c0ec0b494
           <a target="_blank" href={blog.link}rel="nofollow noreferrer">Read Article</a>
